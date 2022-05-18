39ed2fdadd95a62d3c362ed8e270f4f274a5175f
Black <dima.blacker@gmail.com>
2022-05-18 20:39:24 +0300

    test

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -25,19 +25,30 @@ python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 $VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 
 # Run migrations
+echo "$PROJECT_BASE_PATH/profiles-rest-api/"
 cd $PROJECT_BASE_PATH/profiles-rest-api/
+echo "$VIRTUALENV_BASE_PATH/profiles_api/bin/python"
 $VIRTUALENV_BASE_PATH/profiles_api/bin/python app/manage.py migrate
 
 # Setup Supervisor to run our uwsgi process.
+echo '34'
 cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+echo '36'
 supervisorctl reread
+echo '38'
 supervisorctl update
+echo '40'
 supervisorctl restart profiles_api
+echo '42'
 
 # Setup nginx to make our application accessible.
 cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+echo '46'
 rm /etc/nginx/sites-enabled/default
+echo '48'
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+echo '50'
 systemctl restart nginx.service
+echo '53'
 
 echo "DONE! :)"
